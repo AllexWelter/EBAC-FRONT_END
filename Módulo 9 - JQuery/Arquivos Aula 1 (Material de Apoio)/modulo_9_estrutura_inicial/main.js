90d2b4f632e6b77b5055ff0472f2cd9b07d8e990
Allex Welter <allexwelter13@gmail.com>
2024-12-18 07:34:38 -0300

    Update main.js

--- Módulo 9 - JQuery/Arquivos Aula 1 (Material de Apoio)/modulo_9_estrutura_inicial/main.js
+++ Módulo 9 - JQuery/Arquivos Aula 1 (Material de Apoio)/modulo_9_estrutura_inicial/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     console.log(document.querySelector('header button'))
     console.log($('#botao-cancelar')) 
+
+    document.querySelector('header button').addEventListener("click", function() {
+
+    })
+
+    $('header button').click(function() {
+        
+    })
 })
